911a9e94d4f54ce8948d6ca348f7713a598cee69
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-06-05 17:30:22 +0300

    finish video

--- VideoLesson/conditional.ts
+++ VideoLesson/conditional.ts
@@ -1,5 +1,19 @@
 type TypeIsNumber<T> = T extends number ? 'yes' : 'no'
 
-type Type1 = TypeIsNumber<number> 
-type Type2 = TypeIsNumber<string>
+type Type1 = TypeIsNumber<number> //yes
+type Type2 = TypeIsNumber<string> //no
+
+
+// --------------------------- Literal
+
+type TypeBrand ='bmw' | 'mercedes' | 'mclaner'
+type TypePrice = '100$' | '5000$' | '80000$'
+
+type TypeCar = `${TypeBrand}  ${TypePrice}`
+
+const carPrice1:TypeCar = 'bmw  5000$'
+const carPrice2:TypeCar = 'mclaner  80000$'
+
+
+
 
