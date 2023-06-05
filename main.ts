abb9efd49928a6d85d3e5276f9c8b1c87f4d76f6
Svetlana Lytvyn <102797527+0trava@users.noreply.github.com>
2023-06-05 15:51:56 +0300

    add

--- main.ts
+++ main.ts
@@ -22,6 +22,43 @@ const getChannelName:TypeChannelFunction = (name) =>  {
 
 // ----------------------------------------
 
+const getNumbers = (...numbers: number[] ) => {
+
+}
+
+// ---------------------------------------- Перегрузка
+
+function getCar(name:string): string
+function getCar(name: string, price: number): string
+
+function getCar(name: string, price: number): string {
+    return price ? `Название ${name}, Цена ${price}` : `Название ${name}` 
+}
 
 
+const car1 = getCar('bmw')
+const car2 = getCar('bmw', 1000)
+// const car3 = getCar('bmw', 1000, null) - так не працює
+
+// ----------------------------------------Класи
+
+class Car {
+    carName: string
+    price: number
+
+
+    constructor (carName:string, price:number) {
+        this.carName = carName
+        this.price = price
+    }
+
+    getInfo(): string {
+        return `${this.carName} - ${this.price}`
+    }
+}
+
+
+new Car('BMW', 1000).getInfo()
+
+// ---------------------------------------- Интерфейсы
 
